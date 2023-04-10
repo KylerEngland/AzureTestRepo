31a381c51a2eee975cf5e467be6576a8f3b2921a
Kyler England <kylerengland32@gmail.com>
2023-04-10 13:45:06 -0400

    Removed unneccessary function.

--- protected/database.php
+++ protected/database.php
@@ -39,10 +39,10 @@ class database
 
 
 
-//Run the Select query
-printf("Reading data from table: \n");
-$res = mysqli_query($conn, 'SELECT * FROM Products');
-while ($row = mysqli_fetch_assoc($res)) {
-    var_dump($row);
-}
+// //Run the Select query
+// printf("Reading data from table: \n");
+// $res = mysqli_query($conn, 'SELECT * FROM Products');
+// while ($row = mysqli_fetch_assoc($res)) {
+//     var_dump($row);
+// }
 ?>
